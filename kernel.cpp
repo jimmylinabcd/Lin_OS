861b7d1a08950eddd660f7e06f82a92b6522d87e
jimmylinabcd <jimmylinabcd@gmail.com>
2022-01-02 10:50:36 +1000

    updated so the print function can print at a specific location

--- kernel.cpp
+++ kernel.cpp
@@ -11,6 +11,8 @@ link:
 https://github.com/mell-o-tron/MellOs/blob/main/A_Setup/setup-gcc.sh
 NOTE: I experienced  many issues with the setuo and had to do it myself
 
+it's written in C but for somereason it only works when it's a CPP file
+
 Todo list:
 clear function
 keyboard driver
@@ -28,8 +30,6 @@ modules for functions to decluter kernel
 
 
 */
-
-
 
 // VGA text
 #define VID_MEM 0xb8000 //VID_MEM addr increments by 2 to move postion and 1 to switch colour
@@ -55,26 +55,30 @@ modules for functions to decluter kernel
 #define WHITE 15
 
 // Defining functions
-void print(int row, int colour, const char *string);
+void print(int row, int column, int colour, const char *string);
 void clear();
 
 // Need extern for assembly to load kernel
 extern "C" void main(){
     volatile char *postion = (volatile char*)0xB8000; // where the current pointer is in VID_MEM to print afterwards
     
-    print(0, LIGHT_CYAN, "   _      _          ____   _____ ");
-    print(1, LIGHT_CYAN, "  | |    (_)        / __ \\ / ____|");
-    print(2, LIGHT_CYAN, "  | |     _ _ __   | |  | | (___  ");
-    print(3, LIGHT_CYAN, "  | |    | | '_ \\  | |  | |\\___ \\ ");
-    print(4, LIGHT_CYAN, "  | |____| | | | | | |__| |____) |");
-    print(5, LIGHT_CYAN, "  |______|_|_| |_|  \\____/|_____/ ");
+    print(0, 0, LIGHT_CYAN, "   _      _          ____   _____ ");
+    print(1, 0, LIGHT_CYAN, "  | |    (_)        / __ \\ / ____|");
+    print(2, 0, LIGHT_CYAN, "  | |     _ _ __   | |  | | (___  ");
+    print(3, 0,LIGHT_CYAN, "  | |    | | '_ \\  | |  | |\\___ \\ ");
+    print(4, 0, LIGHT_CYAN, "  | |____| | | | | | |__| |____) |");
+    print(5, 0, LIGHT_CYAN, "  |______|_|_| |_|  \\____/|_____/ ");
 
-    print(7, LIGHT_CYAN, "Successful boot! Welcome to Lin OS.");
+    print(7, 0,GREEN, "Successful boot! Welcome to Lin OS.");
+
+    //while(true){}
+    print(9, 0, WHITE, ">>>");
+    print(9, 4, WHITE, "Hello World");
     return;
 }
 
-void print(int row,int colour, const char *string){
-    volatile char *video = (volatile char*)0xB8000 + (row * 160); // 160 because each row is 80 but there is 2 addrs for 1 column meaning 160 in a row
+void print(int row, int column,int colour, const char *string){
+    volatile char *video = (volatile char*)0xB8000 + (row * SCREEN_COLUMNS * 2) + (column * 2); // * 2 because each row has 160 vals because 2 addrs for each location
     while(*string != 0){
         *video++ = *string++;
         *video++ = colour;
@@ -82,6 +86,5 @@ void print(int row,int colour, const char *string){
 }
 
 void clear(){
-    // WIP loop to replace everything
-    print(0,BLACK, " ");
+    // WIP
 }
--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
-nasm "boot.asm" -f bin -o "boot.bin"
-nasm "kernel_entry.asm" -f elf -o "kernel_entry.o"
+export PATH=$PATH:/usr/local/i386elfgcc/bin
 i386-elf-gcc -ffreestanding -m32 -g -c "kernel.cpp" -o "kernel.o"
-nasm "zeros.asm" -f bin -o "zeros.bin"
-
 i386-elf-ld -o "full_kernel.bin" -Ttext 0x1000 "kernel_entry.o" "kernel.o" --oformat binary
-
 cat "boot.bin" "full_kernel.bin" "zeros.bin"  > "OS.bin"
