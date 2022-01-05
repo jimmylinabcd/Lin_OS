6788593a53a8582a534caf755ee1bf116ff9e9f6
jimmylinabcd <jimmylinabcd@gmail.com>
2022-01-05 15:30:51 +1000

    working on interupts

--- kernel.cpp
+++ kernel.cpp
@@ -61,6 +61,31 @@ void scroll();
 void scroll_up();
 void input();
 
+// Interupts
+typedef struct {
+	uint16_t    isr_low;      // The lower 16 bits of the ISR's address
+	uint16_t    kernel_cs;    // The GDT segment selector that the CPU will load into CS before calling the ISR
+	uint8_t     reserved;     // Set to zero
+	uint8_t     attributes;   // Type and attributes; see the IDT page
+	uint16_t    isr_high;     // The higher 16 bits of the ISR's address
+} __attribute__((packed)) idt_entry_t;
+
+__attribute__((aligned(0x10))) 
+static idt_entry_t idt[256]; // Create an array of IDT entries; aligned for performance
+
+typedef struct {
+	uint16_t	limit;
+	uint32_t	base;
+} __attribute__((packed)) idtr_t;
+
+static idtr_t idtr;
+
+__attribute__((noreturn))
+void exception_handler(void);
+void exception_handler() {
+    __asm__ volatile ("cli; hlt"); // Completely hangs the computer
+}
+
 int current_line = 0;
 
 // Need extern for assembly to load kernel
